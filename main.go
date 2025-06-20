73ab70f3d99c271f94675a8160d0c76c861c87ed
sheerazmh <sheerazhaiderkr@gmail.com>
2025-06-20 21:24:40 +1200

    Module 1 Lesson 1.3: Basic Syntax and project simulation

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
+
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, Sheeraz! Go is running on your Machine")
-	fmt.Println("My journey begins")
+	userName := "shirazhaiderkr"
+	userEmail := "shirazhaiderkr@gmail.com"
+	isPremiumUser := true
+	userCredits := 1000
+
+	fmt.Printf("User:%s\n Username: %s\n Are they premium user: %t\n How much credits do they have: %d\n", userName, userEmail, isPremiumUser, userCredits)
+
+	createProject("BlairProject1", "sheerazhaiderkr@gmail.com")
+	createProject("BlairProject2", "sheerazhaiderkr2@gmail.com")
+	simulateGenAICost(20, userCredits)
+	simulateGenAICost(50, userCredits)
+	simulateGenAICost(1000, userCredits)
+
+}
+func createProject(projectName string, projectOwnerEmail string) {
+
+	fmt.Printf("Project %s created by %s\n", projectName, projectOwnerEmail)
+
+}
+
+func simulateGenAICost(createVariationsCount int, userCredits int) {
+
+	totalCost := 5 * createVariationsCount
+
+	if totalCost > userCredits {
+		fmt.Print("Not enough credits\n")
+
+	} else {
+		fmt.Printf("GenAI Produced\n")
+	}
+
 }
